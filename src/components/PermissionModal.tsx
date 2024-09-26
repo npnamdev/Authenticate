dae2506e2df5cd8ed1626bcc10a7b99f552e7801
nam <npnam031101@gmail.com>
2024-09-26 09:33:55 -0500

    update modal past 2

--- src/components/PermissionModal.tsx
+++ src/components/PermissionModal.tsx
@@ -2,8 +2,9 @@
 
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Switch } from "@/components/ui/switch";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 
 export default function PermissionModal({ role }: PermissionModalProps) {
     const [permissions, setPermissions] = useState({
@@ -42,6 +43,33 @@ export default function PermissionModal({ role }: PermissionModalProps) {
                 { key: "deleteUser", label: "Quyền xóa người dùng" },
             ],
         },
+        {
+            name: "Danh sách quyền quản lý blog",
+            permission: [
+                { key: "createPost", label: "Quyền tạo bài viết" },
+                { key: "updatePost", label: "Quyền cập nhật bài viết" },
+                { key: "deletePost", label: "Quyền xóa bài viết" },
+                { key: "getPosts", label: "Quyền xem danh sách bài viết" },
+            ],
+        },
+        {
+            name: "Danh sách quyền sản phẩm",
+            permission: [
+                { key: "createProduct", label: "Quyền tạo sản phẩm" },
+                { key: "updateProduct", label: "Quyền cập nhật sản phẩm" },
+                { key: "deleteProduct", label: "Quyền xóa sản phẩm" },
+                { key: "getProducts", label: "Quyền xem danh sách sản phẩm" },
+            ],
+        },
+        {
+            name: "Danh sách quyền quản lý block",
+            permission: [
+                { key: "createBlock", label: "Quyền tạo block" },
+                { key: "updateBlock", label: "Quyền cập nhật block" },
+                { key: "deleteBlock", label: "Quyền xóa block" },
+                { key: "getBlocks", label: "Quyền xem danh sách block" },
+            ],
+        },
     ];
 
     return (
@@ -54,27 +82,38 @@ export default function PermissionModal({ role }: PermissionModalProps) {
                     <DialogTitle>Edit profile</DialogTitle>
                 </DialogHeader>
                 <div className="">
+
+
                     <h3 className="text-xl font-bold mb-4">{role.name}</h3>
                     <form className="space-y-4">
-                        {permissionList.map((group) => (
-                            <div key={group.name}>
-                                <h4 className="font-semibold text-lg mb-5">{group.name}</h4>
-                                <div className="grid grid-cols-2 gap-5">
-                                    {group.permission.map((permission) => (
-                                        <div key={permission.key} className="flex items-center gap-2">
-                                            <Switch
-                                                id={permission.key}
-                                                checked={permissions[permission.key as keyof typeof permissions]}
-                                                onCheckedChange={() => handlePermissionChange(permission.key)}
-                                            />
-                                            <label htmlFor={permission.key} className="text-sm font-semibold">
-                                                {permission.label}
-                                            </label>
+                        <Accordion defaultValue="item-0" type="single" collapsible>
+                        {permissionList.map((group, index) => (
+                            <AccordionItem key={group.name} value={`item-${index}`}>
+                                <AccordionTrigger>
+                                    <h4 className="">{group.name}</h4>
+                                </AccordionTrigger>
+                                <AccordionContent>
+                                    <div>
+                                        <div className="grid grid-cols-2 gap-5">
+                                            {group.permission.map((permission) => (
+                                                <div key={permission.key} className="flex items-center gap-2">
+                                                    <Switch
+                                                        id={permission.key}
+                                                        checked={permissions[permission.key as keyof typeof permissions]}
+                                                        onCheckedChange={() => handlePermissionChange(permission.key)}
+                                                    />
+                                                    <label htmlFor={permission.key} className="text-sm font-semibold">
+                                                        {permission.label}
+                                                    </label>
+                                                </div>
+                                            ))}
                                         </div>
-                                    ))}
-                                </div>
-                            </div>
+                                    </div>
+                                </AccordionContent>
+                            </AccordionItem>
                         ))}
+                        </Accordion>
+                        
                     </form>
                     <DialogFooter className="border-t mt-5 pt-5">
                         <Button onClick={handleUpdatePermissions} className="text-white">Cập nhật quyền</Button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,9 +21,7 @@ const config: Config = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: '#1213FF',
-  				// DEFAULT: 'hsl(var(--primary))',
-  				// foreground: 'hsl(var(--primary-foreground))'
+  				DEFAULT: '#1213FF'
   			},
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',
@@ -56,6 +54,28 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
